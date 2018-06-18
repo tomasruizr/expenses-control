191e5b8dcb1f046e76ac1ba97b2032c8d4f9893e
Tomás Ruiz <tomasruizr@gmail.com>
2018-06-18 09:31:49 -0300

    Added User

---
+++ src/Users/test/User.test.js
@@ -0,0 +1,6 @@
+describe( 'Users component',() => {
+  it( 'load and diaplays the data of a user from database' );
+  it( 'edits the name field' );
+  it( 'edits the email' );
+  it( 'edits the phone' );
+});
